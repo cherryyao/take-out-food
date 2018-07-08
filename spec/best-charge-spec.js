210cd0f22c37cdf7ae9aa959c895323be4c5ec6c
YAOCH2 <christine.s.yao@oocl.com>
2018-07-08 10:28:00 +0800

    add buildFormattedItem() and buildItemwithDetail()

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,48 +1,67 @@
+const main = require('../src/best-charge');
+const {buildFormattedItem,buildItemwithDetail} = require('../src/best-charge');
+const {loadAllItems} = require('../src/items');
+
 describe('Take out food', function () {
-
-  it('should generate best charge when best is 指定菜品半价', function() {
+  it ('format the inputs--buildFormattedItem()',function(){
     let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
-    let summary = bestCharge(inputs).trim();
-    let expected = `
-============= 订餐明细 =============
-黄焖鸡 x 1 = 18元
-肉夹馍 x 2 = 12元
-凉皮 x 1 = 8元
------------------------------------
-使用优惠:
-指定菜品半价(黄焖鸡，凉皮)，省13元
------------------------------------
-总计：25元
-===================================`.trim()
-    expect(summary).toEqual(expected)
+    let format = buildFormattedItem(inputs);
+    let expected = '[{"id":"ITEM0001","count":1},{"id":"ITEM0013","count":2},{"id":"ITEM0022","count":1}]';
+    expect(JSON.stringify(format)).toEqual(expected);
+
   });
 
-  it('should generate best charge when best is 满30减6元', function() {
-    let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
-    let summary = bestCharge(inputs).trim();
-    let expected = `
-============= 订餐明细 =============
-肉夹馍 x 4 = 24元
-凉皮 x 1 = 8元
------------------------------------
-使用优惠:
-满30减6元，省6元
------------------------------------
-总计：26元
-===================================`.trim()
-    expect(summary).toEqual(expected)
+  it ('detail of items--buildItemwithDetail()',function(){
+    let inputs = [{"id":"ITEM0001","count":1},{"id":"ITEM0013","count":2},{"id":"ITEM0022","count":1}];
+    let itemdetail = buildItemwithDetail(inputs,loadAllItems());
+    let expected = `[{"id":"ITEM0001","name":"黄焖鸡","price":"18.00","count":1,"subtotal":18},{"id":"ITEM0013","name":"肉夹馍","price":"6.00","count":2,"subtotal":12},{"id":"ITEM0022","name":"凉皮","price":"8.00","count":1,"subtotal":8}]`;
+    expect(JSON.stringify(itemdetail)).toEqual(expected);
+
   });
 
-  it('should generate best charge when no promotion can be used', function() {
-    let inputs = ["ITEM0013 x 4"];
-    let summary = bestCharge(inputs).trim();
-    let expected = `
-============= 订餐明细 =============
-肉夹馍 x 4 = 24元
------------------------------------
-总计：24元
-===================================`.trim()
-    expect(summary).toEqual(expected)
-  });
+//   it('should generate best charge when best is 指定菜品半价', function() {
+//     let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+//     let summary = bestCharge(inputs).trim();
+//     let expected = `
+// ============= 订餐明细 =============
+// 黄焖鸡 x 1 = 18元
+// 肉夹馍 x 2 = 12元
+// 凉皮 x 1 = 8元
+// -----------------------------------
+// 使用优惠:
+// 指定菜品半价(黄焖鸡，凉皮)，省13元
+// -----------------------------------
+// 总计：25元
+// ===================================`.trim()
+//     expect(summary).toEqual(expected)
+//   });
+
+//   it('should generate best charge when best is 满30减6元', function() {
+//     let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
+//     let summary = bestCharge(inputs).trim();
+//     let expected = `
+// ============= 订餐明细 =============
+// 肉夹馍 x 4 = 24元
+// 凉皮 x 1 = 8元
+// -----------------------------------
+// 使用优惠:
+// 满30减6元，省6元
+// -----------------------------------
+// 总计：26元
+// ===================================`.trim()
+//     expect(summary).toEqual(expected)
+//   });
+
+//   it('should generate best charge when no promotion can be used', function() {
+//     let inputs = ["ITEM0013 x 4"];
+//     let summary = bestCharge(inputs).trim();
+//     let expected = `
+// ============= 订餐明细 =============
+// 肉夹馍 x 4 = 24元
+// -----------------------------------
+// 总计：24元
+// ===================================`.trim()
+//     expect(summary).toEqual(expected)
+//   });
 
 });
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,54 @@
+const {loadAllItems} = require('../src/items');
+
 function bestCharge(selectedItems) {
-  return /*TODO*/;
+  
+  const formarttedItems = buildFormattedItem(selectedItems);
+  buildItemwithDetail(formarttedItems,loadAllItems());
+
+
+
+}
+
+//1.格式化菜品
+function buildFormattedItem(inputs){
+let tempArray=[];
+let formarttedItems=[];
+for(let item of inputs){
+   tempArray = item.split("x");
+   formarttedItems.push({
+     id:tempArray[0].trim(),
+     count:parseFloat(tempArray[1])
+   });
+}
+console.info(formarttedItems);
+return formarttedItems;
+}
+
+
+//2.生成商品详情
+function buildItemwithDetail(formarttedItems,AllItems){
+   let ItemDetails=[];
+   for(let formarttedItem of formarttedItems)
+   {
+     for(let item of AllItems){
+      if(formarttedItem.id === item.id){
+        ItemDetails.push({
+          id:formarttedItem.id,
+          name:item.name,
+          price: item.price.toFixed(2),
+          count:formarttedItem.count,
+          subtotal:item.price*formarttedItem.count
+        });
+      }
+     }
+   }
+   console.info(ItemDetails);
+   return ItemDetails;
+}
+
+
+
+module.exports = {
+  buildFormattedItem,
+  buildItemwithDetail
 }
